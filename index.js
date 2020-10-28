4e29a92f93226f68c5c619e074bc30a4e630ef3a
trevismcgill <trevismcgill25@gmail.com>
2020-10-28 13:30:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const names = ["bob", "george", "mark"]
+
+function writeCards(a, b) {
+    let c = [];
+    for (let i = 0; i < a.length; i++) {
+        c.push(`Thank you, ${a[i]}, for the wonderful ${b} gift!`);
+    };
+    return c;
+};
+
+function countDown(i) {
+    
+    while (i > -1) {
+        console.log(i);
+        i--;
+    };
+
+};
